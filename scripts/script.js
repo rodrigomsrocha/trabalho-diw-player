17fd36b70ac47106236effa712fa61f77f52e25c
rodrigomsrocha <rodrigomtc82@gmail.com>
2023-09-15 23:46:59 -0300

    feature: hide controls on mouse idle

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,25 @@ const playBtnIcon = document.querySelector(".play-btn-icon");
 const currentTime = document.querySelector(".current-time");
 const duration = document.querySelector(".duration");
 const progressBar = document.querySelector(".progress-bar");
+const footer = document.querySelector("footer");
 
 let audioState = "play";
+let idleState = false;
+let idleTimer = null;
+
+function showFooter(time) {
+  clearTimeout(idleTimer);
+
+  if (idleState === true) {
+    footer.classList.remove("inactive");
+  }
+  idleState = false;
+
+  idleTimer = setTimeout(() => {
+    footer.classList.add("inactive");
+    idleState = true;
+  }, time);
+}
 
 function calculateTime(duration) {
   const minutes = Math.floor(duration / 60);
@@ -24,6 +41,12 @@ if (audio.readyState < 0) {
     displayDuration();
   });
 }
+
+showFooter(5000);
+
+window.addEventListener("mousemove", () => {
+  showFooter(3000);
+});
 
 audio.addEventListener("timeupdate", () => {
   progressBar.style.width = `${(audio.currentTime / audio.duration) * 100}%`;
